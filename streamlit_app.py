3778bdc5d1ee28444baaa5993735da28641f90bd
Luciana Silveira <lucianasilveira007@gmail.com>
2026-01-18 22:46:55 +0000

    primeiro commit da atividade

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
 #substituir balaão por iconme de escrita
-
-st.title("🎈 Criador de  textos")
-st.header("Criador de textos como monólogos personalizados com o seu sentimento, seu gênero textual, sua idade e tema")
+)
 st.subheader("Drama, Comédia, Fição Científica, Fantasia, Suspense, Terror ou Romance")
 st.write("Após incluir esses itens, você terá um monologo personalizado criado por você. Gostou? Então, aproveite!"
 )
